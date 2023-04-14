59837d5b089436dfe2fccf5570753f8b81febf1a
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-04-14 16:41:30 +0200

    Add buttons to layout (inactive)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function createGrid () {
     for (i = 1; i <= 256; ++i) {
         let createDiv = document.createElement("div");
         createDiv.classList.add("pixel");
-        createDiv.addEventListener("mouseover", () => createDiv.classList.add("colored"));
+        createDiv.addEventListener("mouseover", () => createDiv.classList.add("black"));
         grid.appendChild(createDiv);
     }
 }
