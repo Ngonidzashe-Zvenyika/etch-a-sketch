c553f5951b4db0bce370545f81e3a615eb6dd2f7
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-04-18 22:54:28 +0200

    Remove overlapping event listeners and add shading feature

--- script.js
+++ script.js
@@ -1,73 +1,48 @@
 // These are the initialized global variables;
-const gridSize = 640;
+let black = false;
+let rainbow = false;
+let erase = false;
+let clear = false;
+const gridSize = 640; 
 let squaresPerSide = 20;
 let numberSquares = squaresPerSide*squaresPerSide;
 const grid = document.querySelector(".grid");
 const buttons = document.querySelectorAll("button");
 
-
-// This function addes squares to the main div;
-function createNewGrid() {
-    for (i = 1; i <= numberSquares; ++i) {
-        const createSquare = document.createElement("div");
-        const squareSize = `${gridSize/squaresPerSide}px`;
-        createSquare.classList.add("square");
-        createSquare.style.width = squareSize;
-        createSquare.style.height = squareSize;
-        grid.appendChild(createSquare);
-    }
-}  
-
-// This function removes existing squares from the main div when a new grid is being created;
-function removeGrid() {
-    const squares = document.querySelectorAll(".square");
-    for (const square of squares) {
-        square.remove();
-    }
-}
-
-// This function changes the brush color to black;
-function blackBrush() {
-    const squares = document.querySelectorAll(".square");
-    for (const square of squares) {
-        square.addEventListener("mouseover", ()=> {
-            square.style.backgroundColor = `rgb(0, 0, 0)`;
+// This function detects which button has been pressed and effects changes to suit the button's purpose;
+function buttonChoice() {
+    for (const button of buttons) {
+        button.addEventListener("click", ()=> {
+            switch (true) {
+            case (button.classList.contains("gridSizeButton")):
+                enterGridSize();
+                break;
+            case (button.classList.contains("blackButton")):
+                black = true;
+                rainbow = false;
+                erase = false;
+                break;
+            case (button.classList.contains("rainbowButton")):
+                rainbow = true;
+                black = false;
+                erase = false;
+                break;
+            case (button.classList.contains("eraserButton")):
+                erase = true;
+                black = false;
+                rainbow = false;
+                break;
+            case (button.classList.contains("clearButton")):
+                clearGrid();
+                erase = false;
+                black = false;
+                rainbow = false;
+            }
         });
     }
 }
 
-// This function generates a random brush color above each square;
-function rainbowBrush() {
-    const squares = document.querySelectorAll(".square");
-    for (const square of squares) {
-        square.addEventListener("mouseover", ()=> {
-            let x = Math.random() * 256;
-            let y = Math.random() * 256;
-            let z = Math.random() * 256;
-            square.style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
-        });
-    }
-}
-
-// This function changes the brush color to white in order to "erase" individual squares;
-function eraseColor() {
-    const squares = document.querySelectorAll(".square");
-    for (const square of squares) {
-        square.addEventListener("mouseover", ()=> {
-            square.style.backgroundColor = `rgb(255, 255, 255)`;
-        });
-    }
-}
-
-// This function reverts the color of all the squares back to whtie;
-function clearGrid() {
-    const squares = document.querySelectorAll(".square");
-    for (const square of squares) {
-        square.style.backgroundColor = `rgb(255, 255, 255)`;
-    }
-}
-
-// This function requests and validates an input for the grid size, invalid entries prevent the body from executing.
+// This function requests and validates an input for the grid size;
 function enterGridSize() {
     let valid = false;
     let input = prompt("How many squares per side? (Maximum: 64)");
@@ -90,29 +65,71 @@ function enterGridSize() {
     createNewGrid();
 }
 
-// This event listener determines which function to call based on what button the user has pressed;
-for (const button of buttons) {
-    button.addEventListener("click", ()=> {
-        switch (true) {
-        case (button.classList.contains("gridSizeButton")):
-            enterGridSize();
-            break;
-        case (button.classList.contains("blackButton")):
-            blackBrush();
-            break;
-        case (button.classList.contains("rainbowButton")):
-            rainbowBrush();
-            break;
-        case (button.classList.contains("eraserButton")):
-            eraseColor();
-            break;
-        case (button.classList.contains("clearButton")):
-            clearGrid();
-            break;
-        }
-    });
+// This function removes existing squares from the main div when a new grid is being created;
+function removeGrid() {
+    const squares = document.querySelectorAll(".square");
+    for (const square of squares) {
+        square.remove();
+    }
+}
+
+// This function addes squares to the main div and calls brushChoice();
+function createNewGrid() {
+    for (i = 1; i <= numberSquares; ++i) {
+        const createSquare = document.createElement("div");
+        const squareSize = `${gridSize/squaresPerSide}px`;
+        createSquare.classList.add("square");
+        createSquare.style.width = squareSize;
+        createSquare.style.height = squareSize;
+        grid.appendChild(createSquare);
+    }
+    brushChoice();
+}
+
+// This function replaces the entire grid with a new blank grid of equal dimension;
+function clearGrid() {
+    removeGrid();
+    createNewGrid();
+}
+
+// This function adds an event listener to each square and governs all color changes cuased by "mouseover" events;
+function brushChoice() {
+    const squares = document.querySelectorAll(".square");
+    for (const square of squares) {
+        let intensity = 0.1;
+        square.addEventListener("mouseover", ()=> {
+            if (black === true) {
+                blackBrush(square, intensity);
+                    if (intensity < 1) intensity += 0.1;
+            } else if (rainbow === true) {
+                intensity = 0.1;
+                rainbowBrush(square);
+            } else if (erase === true) {
+                intensity = 0.1;
+                eraseColor(square);
+            }
+        });
+    }
+}
+
+// This function gradually shades a square by adding 10% of the color black upon each pass;
+function blackBrush(square, intensity) {
+    square.style.backgroundColor = `rgb(0, 0, 0, ${intensity})`;
+}
+
+// This function generates a random brush color above each square;
+function rainbowBrush(square) {
+    let x = Math.random() * 256;
+    let y = Math.random() * 256;
+    let z = Math.random() * 256;
+    square.style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
+}
+
+// This function changes the brush color to white in order to "erase" individual squares;
+function eraseColor(square) {
+    square.style.backgroundColor = `rgb(255, 255, 255)`;
 }
 
 // Main program;
 createNewGrid();
-
+buttonChoice();
